9d7b0b3ad2b7fc903b8f0475e1f579d2061b6f9a
horiba <suizlet@gmail.com>
2023-07-09 16:33:37 +0900

    fix: タイトルを修正

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -8,7 +8,55 @@
   </v-app>
 </template>
 
-<script setup>
+<script>
   import GlobalHeader from './Header.vue'
   import GlobalFooter from './Footer.vue'
+  const SITE_NAME = 'k-bushi.com'
+  export default {
+    name: 'app',
+    data() {
+      return {
+        items: []
+      }
+    },
+    components: {
+      GlobalHeader,
+      GlobalFooter
+    },
+    created() {
+      this.$router.options.routes.forEach((route, index) => {
+        this.items.push({
+          id: index,
+          name: route.name,
+          path: route.path,
+          title: route.title
+        })
+      })
+    },
+    methods: {
+      initPageTitle() {
+        document.title = SITE_NAME
+      },
+      createPageTitle() {
+        if (this.activeTitle) {
+          document.title = this.activeTitle + ' - ' + SITE_NAME
+        } else {
+          document.title = SITE_NAME
+        }
+      }
+    },
+    computed: {
+      activeTitle() {
+        const item = this.items.find(item => {
+          return item.name === this.$route.name
+        })
+        return item.title
+      }
+    },
+    watch: {
+      $route() {
+        this.createPageTitle()
+      }
+    }
+  }
 </script>
