c7c76087cf3a61a5eec3b7f7fa13c6e12a0bc5c8
horiba <suizlet@gmail.com>
2023-07-30 14:42:49 +0900

    refactor: ProfileをcompositionAPIに変更する

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -80,123 +80,120 @@
   </v-container>
 </template>
 
-<script>
-// timelineを使用する
-export default {
-  name: 'Profile',
-  data() {
-    return {
-      qualifications: [
-        {
-          id: 1,
-          name: '基本情報技術者',
-          dotColor: 'blue-grey-darken-4',
-          date: '2014/04/20',
-          impression:
-            '１回目は大学2年の秋に受験をしたが、ろくに試験対策をせずに受験し午前試験で落ちた。この試験は大学4年の春に受験し、午前午後対策をきっちりとし合格した。'
-        },
-        {
-          id: 2,
-          name: '応用情報技術者',
-          dotColor: 'blue-grey-darken-4',
-          date: '2015/04/19',
-          impression:
-            '１回目は大学4年の秋に受験をしたが落ちた、午後試験で３０点近くしか取れておらず記述問題が壊滅的であったからだ。この試験は社会人1年目の春に受験し、まだ研修中であったことから退社後にもきっちりとした勉強時間が午後対策がしっかりでき合格することができた。'
-        },
-        {
-          id: 3,
-          name: 'AWS Certified Solutions Architect - Associate (SAA)',
-          dotColor: 'pink-darken-4',
-          date: '2020/10/29',
-          impression:
-            'ここ数年でAWSを触る機会が多くなったため、知識の整理として受験。1ヶ月ほど勉強して合格することができた。',
-          link: 'https://www.credly.com/badges/4a0494ed-fddf-4bb5-976f-d337ff78d001'
-        },
-        {
-          id: 4,
-          name: 'AWS Certified Developer - Associate (DVA)',
-          dotColor: 'pink-darken-4',
-          date: '2021/09/10',
-          impression:
-            '業務ではAWSでの開発は経験がないが、去年度に取得したSAAの次の資格として取得した。こちらも1ヶ月ほどの勉強で合格をした。',
-          link: 'https://www.credly.com/badges/23441bc1-7790-404c-9039-6101941e7076'
-        },
-        {
-          id: 5,
-          name: 'AWS Certified Solutions Architect - Professional (SAP)',
-          dotColor: 'pink-darken-4',
-          date: '2022/07/01',
-          impression:
-            'AWSの業務を任されることが多くなってきたため、体系的に学習をして業務にてより合ったAWSのサービスを選定できるようになりたいと思ったため受験をした。また、去年度に取得したDVAの次の資格として取得した。2ヶ月ほどの勉強で合格をした。(結構危うかった…)',
-          link: 'https://www.credly.com/badges/8b1084b6-6bb7-41a3-b826-6e70c109201d'
-        },
-        {
-          id: 6,
-          name: 'AWS Certified SysOps Administrator - Associate (SOA)',
-          dotColor: 'pink-darken-4',
-          date: '2022/07/28',
-          impression:
-            'AWS DOPを取る前に取っておきたいのと、ラボ試験がどういうものかというのを見てみたという興味で受験をした。2週間ほどの勉強で合格した。',
-          link: 'https://www.credly.com/badges/b54b49f5-a909-4955-9e8b-768d5ec21ea7'
-        },
-        {
-          id: 7,
-          name: 'AWS Certified DevOps Engineer - Professional (DOP)',
-          dotColor: 'pink-darken-4',
-          date: '2022/12/14',
-          impression:
-            'DVA/SOAの次の資格としてプロフェッショナル資格の最後の試験として受験をした。1ヶ月ほどの勉強で合格した。',
-          link: 'https://www.credly.com/badges/ffcb68e6-01fb-4dc7-b360-6e1c3ff69b77'
-        },
-        {
-          id: 8,
-          name: '3級FP技能検定',
-          dotColor: 'purple-darken-4',
-          date: '2023/01/22',
-          impression:
-            '合格発表日は 2023/03/02. 投資で有利になるかと思ったのと、お金の勉強をしたかったので取ってみた。'
-        },
-        {
-          id: 9,
-          name: 'ネットワークスペシャリスト',
-          dotColor: 'blue-grey-darken-4',
-          date: '2023/04/16',
-          impression:
-            '合格発表日は 2023/06/29 4回目で合格、高度試験初合格でやっとという感じ。しっかり試験対策したので合格できた。'
-        },
-        {
-          id: 10,
-          name: '2級FP技能検定',
-          dotColor: 'purple-darken-4',
-          date: '2023/05/28',
-          impression:
-            '合格発表日は 2023/07/04. FP3級の次なるステップとして勉強をしたかったので取ってみた。'
-        },
-        {
-          id: 11,
-          name: '簿記検定3級',
-          dotColor: 'amber-darken-4',
-          date: '2023/07/28',
-          impression:
-            'CBTで受験。データベーススペシャリストの問題への理解と、企業調査で何かと役立つかと思い基本知識として取った。'
-        },
-      ],
-      challenges: [
-        { id: 1, name: 'データベーススペシャリストの取得' },
-        { id: 2, name: 'OSS開発の貢献' },
-        { id: 3, name: '情報処理安全確保支援士の合格' },
-      ]
-    }
+<script setup>
+import { ref, computed } from 'vue';
+
+const qualifications = ref([])
+const challenges = ref([])
+
+qualifications.value.push(
+  {
+    id: 1,
+    name: '基本情報技術者',
+    dotColor: 'blue-grey-darken-4',
+    date: '2014/04/20',
+    impression:
+      '１回目は大学2年の秋に受験をしたが、ろくに試験対策をせずに受験し午前試験で落ちた。この試験は大学4年の春に受験し、午前午後対策をきっちりとし合格した。'
   },
-  computed: {
-    age() {
-      let date = new Date()
-      let birth = new Date('1992-11-23')
-      let diff = Math.floor((date - birth) / 86400000 / 365)
-      return diff
-    }
-  }
-}
+  {
+    id: 2,
+    name: '応用情報技術者',
+    dotColor: 'blue-grey-darken-4',
+    date: '2015/04/19',
+    impression:
+      '１回目は大学4年の秋に受験をしたが落ちた、午後試験で３０点近くしか取れておらず記述問題が壊滅的であったからだ。この試験は社会人1年目の春に受験し、まだ研修中であったことから退社後にもきっちりとした勉強時間が午後対策がしっかりでき合格することができた。'
+  },
+  {
+    id: 3,
+    name: 'AWS Certified Solutions Architect - Associate (SAA)',
+    dotColor: 'pink-darken-4',
+    date: '2020/10/29',
+    impression:
+      'ここ数年でAWSを触る機会が多くなったため、知識の整理として受験。1ヶ月ほど勉強して合格することができた。',
+    link: 'https://www.credly.com/badges/4a0494ed-fddf-4bb5-976f-d337ff78d001'
+  },
+  {
+    id: 4,
+    name: 'AWS Certified Developer - Associate (DVA)',
+    dotColor: 'pink-darken-4',
+    date: '2021/09/10',
+    impression:
+      '業務ではAWSでの開発は経験がないが、去年度に取得したSAAの次の資格として取得した。こちらも1ヶ月ほどの勉強で合格をした。',
+    link: 'https://www.credly.com/badges/23441bc1-7790-404c-9039-6101941e7076'
+  },
+  {
+    id: 5,
+    name: 'AWS Certified Solutions Architect - Professional (SAP)',
+    dotColor: 'pink-darken-4',
+    date: '2022/07/01',
+    impression:
+      'AWSの業務を任されることが多くなってきたため、体系的に学習をして業務にてより合ったAWSのサービスを選定できるようになりたいと思ったため受験をした。また、去年度に取得したDVAの次の資格として取得した。2ヶ月ほどの勉強で合格をした。(結構危うかった…)',
+    link: 'https://www.credly.com/badges/8b1084b6-6bb7-41a3-b826-6e70c109201d'
+  },
+  {
+    id: 6,
+    name: 'AWS Certified SysOps Administrator - Associate (SOA)',
+    dotColor: 'pink-darken-4',
+    date: '2022/07/28',
+    impression:
+      'AWS DOPを取る前に取っておきたいのと、ラボ試験がどういうものかというのを見てみたという興味で受験をした。2週間ほどの勉強で合格した。',
+    link: 'https://www.credly.com/badges/b54b49f5-a909-4955-9e8b-768d5ec21ea7'
+  },
+  {
+    id: 7,
+    name: 'AWS Certified DevOps Engineer - Professional (DOP)',
+    dotColor: 'pink-darken-4',
+    date: '2022/12/14',
+    impression:
+      'DVA/SOAの次の資格としてプロフェッショナル資格の最後の試験として受験をした。1ヶ月ほどの勉強で合格した。',
+    link: 'https://www.credly.com/badges/ffcb68e6-01fb-4dc7-b360-6e1c3ff69b77'
+  },
+  {
+    id: 8,
+    name: '3級FP技能検定',
+    dotColor: 'purple-darken-4',
+    date: '2023/01/22',
+    impression:
+      '合格発表日は 2023/03/02. 投資で有利になるかと思ったのと、お金の勉強をしたかったので取ってみた。'
+  },
+  {
+    id: 9,
+    name: 'ネットワークスペシャリスト',
+    dotColor: 'blue-grey-darken-4',
+    date: '2023/04/16',
+    impression:
+      '合格発表日は 2023/06/29 4回目で合格、高度試験初合格でやっとという感じ。しっかり試験対策したので合格できた。'
+  },
+  {
+    id: 10,
+    name: '2級FP技能検定',
+    dotColor: 'purple-darken-4',
+    date: '2023/05/28',
+    impression:
+      '合格発表日は 2023/07/04. FP3級の次なるステップとして勉強をしたかったので取ってみた。'
+  },
+  {
+    id: 11,
+    name: '簿記検定3級',
+    dotColor: 'amber-darken-4',
+    date: '2023/07/28',
+    impression:
+      'CBTで受験。データベーススペシャリストの問題への理解と、企業調査で何かと役立つかと思い基本知識として取った。'
+  },
+)
+
+challenges.value.push(
+  { id: 1, name: 'データベーススペシャリストの取得' },
+  { id: 2, name: 'OSS開発の貢献' },
+  { id: 3, name: '情報処理安全確保支援士の合格' },
+)
+
+const age = computed(()=> {
+  let date = new Date()
+  let birth = new Date('1992-11-23')
+  let diff = Math.floor((date - birth) / 86400000 / 365)
+  return diff
+})
 </script>
 
 <style scoped>
